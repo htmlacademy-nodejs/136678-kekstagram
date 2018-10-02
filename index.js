ab26b4b4ace8ce7bca165b2fff24ab9b8dd3ba73
Maria Filippova <maria.filippova@sibers.com>
2018-10-02 21:48:01 +0700

    Initiated project and fixed style errors

--- index.js
+++ index.js
@@ -1,36 +1,60 @@
+'use strict';
+
 const Project = {
   VERSION: `v0.0.1`,
   NAME: `Kekstagram`,
   AUTHOR: `Maria Filippova`,
 };
 
+const CommandsName = {
+  HELP: `--help`,
+  VERSION: `--version`,
+};
+
 const command = process.argv[2];
 
+const commands = {
+  [CommandsName.HELP]: {
+    info: `Доступные команды:\n--help    — печатает этот текст;\n--version — печатает версию приложения;`,
+    action() {
+      console.log(this.info);
+    }
+  },
+  [CommandsName.VERSION]: {
+    info: Project.VERSION,
+    action() {
+      console.log(this.info);
+    }
+  },
+  default: {
+    info: `Привет пользователь!\nЭта программа будет запускать сервер «${Project.NAME}».\nАвтор: ${Project.AUTHOR}.`,
+    action() {
+      console.log(this.info);
+    }
+  },
+  error: {
+    info: `Неизвестная команда "${command}".\nЧтобы прочитать правила использования приложения, наберите "--help"`,
+    action() {
+      console.log(this.info);
+    }
+  },
+};
+
+
 switch (command) {
-  case `--help`:
-    console.log(
-      `Доступные команды:\n` +
-      `--help    — печатает этот текст;\n` +
-      `--version — печатает версию приложения;`
-    );
-    process.exit();
-
-  case `--version`:
-    console.log(Project.VERSION);
-    process.exit();
-
+  case CommandsName.HELP:
+    commands[command].action();
+    process.exit(0);
+    break;
+  case CommandsName.VERSION:
+    commands[command].action();
+    process.exit(0);
+    break;
   case undefined:
-    console.log(
-      `Привет пользователь!\n` +
-      `Эта программа будет запускать сервер «${Project.NAME}».\n` +
-      `Автор: ${Project.AUTHOR}.`
-    );
-    process.exit();
-
+    commands.default.action();
+    process.exit(0);
+    break;
   default:
-    console.error(
-      `Неизвестная команда "${command}".\n` +
-      `Чтобы прочитать правила использования приложения, наберите "--help"`
-    );
+    commands.error.action();
     process.exit(1);
 }
